411ce9d1bcbae33faffdfd0eeb0cc31dddb1911e
smehdii <siddik.mehdi@gmail.com>
2019-04-12 08:56:43 +0200

    Finish Setting up of NameBoard

--- src/components/NameBoard/NameBoard.js
+++ src/components/NameBoard/NameBoard.js
@@ -1,12 +1,31 @@
 import PropTypes from "prop-types"
 import React from "react"
-// import { HighlightBGPrimary } from '@components/Highlight'
+import { HighlightBGPrimary } from "../Highlight"
 import SocialHandles from "../SocialHandles"
-// import styles from './NameBoard.module.scss'
+import styles from "./NameBoard.module.scss"
 import { StaticQuery, graphql } from "gatsby"
 
 const NameBoard = ({ siteMetadata }) => {
-  return <SocialHandles siteMetadata={siteMetadata} />
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.name}>{siteMetadata.fullName}</h1>
+      <div className={styles.subWrapper}>
+        <div className={styles.sub}>
+          <HighlightBGPrimary withPad>Creator</HighlightBGPrimary>
+        </div>
+        <div className={styles.dotSeparator} />
+        <div className={styles.sub}>
+          <HighlightBGPrimary withPad>Writer</HighlightBGPrimary>
+        </div>
+        <div className={styles.dotSeparator} />
+        <div className={styles.sub}>
+          <HighlightBGPrimary withPad>Engineer</HighlightBGPrimary>
+        </div>
+      </div>
+      <SocialHandles siteMetadata={siteMetadata} />
+      <div className={styles.separator} />
+    </div>
+  )
 }
 
 NameBoard.propTypes = {
